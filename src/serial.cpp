cd9b34461348c306c44475c3b8f334662b94208b
Vladimir Still <vl.still@gmail.com>
2018-10-08 15:33:46 +0200

    Fix serial output of integers.

--- src/serial.cpp
+++ src/serial.cpp
@@ -10,10 +10,22 @@ Serial &Serial::operator<<( const char *v ) {
     return *this;
 }
 
-Serial &Serial::operator<<( char v ) {
-    ::Serial.print( v );
-    return *this;
+#define NUM_OUT( T ) Serial &Serial::operator<<( T v ) { \
+    ::Serial.print( v ); \
+    return *this; \
 }
+
+NUM_OUT( char )
+
+NUM_OUT( short )
+NUM_OUT( int )
+NUM_OUT( long )
+
+NUM_OUT( unsigned short )
+NUM_OUT( unsigned )
+NUM_OUT( unsigned long )
+
+#undef NUM_OUT
 
 Serial &Serial::operator>>( char &v ) {
     while ( ::Serial.available() == 0 ) { }
--- src/serial.hpp
+++ src/serial.hpp
@@ -6,6 +6,14 @@ struct Serial {
     Serial &operator<<( const char *v );
     Serial &operator<<( char v );
 
+    Serial &operator<<( short v );
+    Serial &operator<<( int v );
+    Serial &operator<<( long v );
+
+    Serial &operator<<( unsigned short v );
+    Serial &operator<<( unsigned v );
+    Serial &operator<<( unsigned long v );
+
     Serial &operator>>( char &v );
 };
 

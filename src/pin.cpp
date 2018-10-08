ced18afe7c7a04870d0b50878673a41196664cfd
Vladimir Still <vl.still@gmail.com>
2018-10-08 13:25:08 +0200

    Rewrite digital output using low-level interface.

--- src/pin.cpp
+++ src/pin.cpp
@@ -3,12 +3,36 @@
 namespace axx {
     namespace raw {
 
+        using portreg = volatile uint8_t &;
+
+#define PIN_MAP(out_t, to, B, C, D) static out_t pinTo ## to ( uint8_t pin ) { \
+            switch ( pin ) { \
+                case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: \
+                    return D; \
+                case 8: case 9: case 10: case 11: case 12: case 13: \
+                    return B; \
+                case 14: case 15: case 16: case 17: case 18: case 19: \
+                    return C; \
+            } \
+            return *((uint8_t *)0); \
+        } \
+
+        PIN_MAP( portreg, Ddr, DDRB, DDRC, DDRD )
+        PIN_MAP( portreg, Port, PORTB, PORTC, PORTD )
+        PIN_MAP( portreg, Pin, PINB, PINC, PIND )
+        PIN_MAP( uint8_t, Bit, pin, pin - 8, pin - 14 )
+#undef PIN_MAP
+
         void setInput( uint8_t pin ) {
-            pinMode( pin, OUTPUT );
+            auto &reg = pinToDdr( pin );
+            const auto mask = 1 << pinToBit( pin );
+            reg &= ~mask;
         }
 
         void setOutput( uint8_t pin ) {
-            pinMode( pin, INPUT );
+            auto &reg = pinToDdr( pin );
+            const auto mask = 1 << pinToBit( pin );
+            reg |= mask;
         }
 
         unsigned long pulseIn( uint8_t pin, bool level, unsigned long timeout ) {
@@ -17,7 +41,18 @@ namespace axx {
 
         namespace digital {
             void write( uint8_t pin, bool value ) {
-                digitalWrite( pin, value ? HIGH : LOW );
+                auto &reg = pinToPort( pin );
+                const auto mask = 1 << pinToBit( pin );
+                if ( value ) {
+                    reg |= mask;
+                }
+                else {
+                    reg &= ~mask;
+                }
+            }
+
+            void flip( uint8_t pin ) {
+                pinToPin( pin ) |= 1 << pinToBit( pin );
             }
         }
 

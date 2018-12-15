d75c5907600640f0716ee06d3139cea6a1e1edd2
Vladimir Still <vl.still@gmail.com>
2018-12-15 14:45:26 +0100

    Read line sensor and dump its status to LEDS using a shift register.

--- esp32/main.cpp
+++ esp32/main.cpp
@@ -1,16 +1,18 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "driver/mcpwm.h"
-#include "soc/soc.h"
-#include "soc/mcpwm_struct.h"
-#include "soc/mcpwm_reg.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include <driver/mcpwm.h>
+#include <soc/soc.h>
+#include <soc/mcpwm_struct.h>
+#include <soc/mcpwm_reg.h>
+#include <driver/spi_master.h>
 #include "sdkconfig.h"
 #include <chrono>
 #include <thread>
 #include "motor.hpp"
 #include "ultrasonic.hpp"
 #include <atomic>
+#include <experimental/iterator>
 
 static_assert( ATOMIC_INT_LOCK_FREE );
 
@@ -23,8 +25,12 @@ constexpr FreqHz MOTOR_MWM_FREQ = 50_Hz;
 
 constexpr gpio_num_t ULTRASONIC_TRIG = 18_pin;
 constexpr static auto ULTRASONIC_TRIG_CHANNEL = RMT_CHANNEL_0;
+constexpr static int ULTRASONIC_TRIG_DIV = 128;
+constexpr static auto ULTRASONIC_TRIG_TICK = BASE_TICK * ULTRASONIC_TRIG_DIV;
+
 constexpr gpio_num_t ULTRASONIC_ECHO = 5_pin;
 constexpr auto ULTRASONIC_CAPTURE = MCPWM_CAP_0;
+
 constexpr auto CAP0_INT_EN = BIT(27);
 
 // motor1: 23, 22
@@ -48,10 +54,11 @@ struct UltrasonicDist {
     std::atomic< uint32_t > cnt{ 0 };
 };
 
-rmt_item32_t ultrasonic_trig_signal;
+rmt_item32_t ultrasonic_trig_signal[2];
 
-void ultrasonic_trig() {
-	rmt_write_items( ULTRASONIC_TRIG_CHANNEL, &ultrasonic_trig_signal, 1, false /* do not wait */ );
+void ultrasonic_trig( bool initial ) {
+	rmt_write_items( ULTRASONIC_TRIG_CHANNEL, &ultrasonic_trig_signal[ initial ], 2 - initial,
+                     false /* do not wait */ );
 }
 
 void capture_handler( void *arg ) {
@@ -66,10 +73,11 @@ void capture_handler( void *arg ) {
         }
         else { // down edge
             auto end = mcpwm_capture_signal_get_value( MCPWM_UNIT_0, MCPWM_SELECT_CAP0 );
-            dist.distance = end - dist.start;
+            dist.distance = std::chrono::duration_cast< std::chrono::milliseconds >(
+                              (( end - dist.start ) * BASE_TICK * 340) / 2 ).count();
             dist.cnt++;
             mcpwm_capture_enable( MCPWM_UNIT_0, MCPWM_SELECT_CAP0, MCPWM_POS_EDGE, 0 );
-            ultrasonic_trig();
+            ultrasonic_trig( false );
         }
     }
     MCPWM0.int_clr.val = status; // handled
@@ -79,21 +87,26 @@ void ultrasonic_measure_continuosly( UltrasonicDist &dist ) {
     MCPWM0.int_ena.val = CAP0_INT_EN;
     mcpwm_isr_register( MCPWM_UNIT_0, capture_handler, &dist, 0 /* ESP_INTR_FLAG_IRAM */, nullptr );
     mcpwm_capture_enable( MCPWM_UNIT_0, MCPWM_SELECT_CAP0, MCPWM_POS_EDGE, 0 );
-    ultrasonic_trig();
+    ultrasonic_trig( true );
 }
 
 void ultrasonic_init_trig() {
-    ultrasonic_trig_signal.duration0 = 1 + 100us / BASE_TICK;
-    ultrasonic_trig_signal.level0 = 1;
-    ultrasonic_trig_signal.duration1 = 1;
-    ultrasonic_trig_signal.level1 = 0;
+    // do not measure faster than every 100ms
+    ultrasonic_trig_signal[0].duration0 = 50ms / ULTRASONIC_TRIG_TICK;
+    ultrasonic_trig_signal[0].level0 = 0;
+    ultrasonic_trig_signal[0].duration1 = 50ms / ULTRASONIC_TRIG_TICK;
+    ultrasonic_trig_signal[0].level1 = 0;
+    ultrasonic_trig_signal[1].duration0 = 1 + 20us / ULTRASONIC_TRIG_TICK;
+    ultrasonic_trig_signal[1].level0 = 1;
+    ultrasonic_trig_signal[1].duration1 = 1;
+    ultrasonic_trig_signal[1].level1 = 0;
 
     rmt_config_t trig_config;
     trig_config.rmt_mode = RMT_MODE_TX;
     trig_config.channel = ULTRASONIC_TRIG_CHANNEL;
     trig_config.gpio_num = ULTRASONIC_TRIG;
     trig_config.mem_block_num = 1;
-    trig_config.clk_div = 1;
+    trig_config.clk_div = ULTRASONIC_TRIG_DIV;
     trig_config.tx_config.loop_en = false;
     trig_config.tx_config.carrier_en = false; // no modulation
     trig_config.tx_config.idle_output_en = true; // when idle…
@@ -102,6 +115,121 @@ void ultrasonic_init_trig() {
     rmt_config( &trig_config ) || Die( "Ultrasonic: trigger RMT failed to config" );
     rmt_driver_install( trig_config.channel, 0, 0 )
         || Die( "Ultrasonic: trigger RMT failed in driver install" );
+}
+
+constexpr gpio_num_t LINE_CS = 14_pin;
+constexpr gpio_num_t LINE_MOSI = 27_pin;
+constexpr gpio_num_t LINE_MISO = 26_pin;
+constexpr gpio_num_t LINE_SCK = 25_pin;
+
+constexpr gpio_num_t LED_FEEDER_SCK = 0_pin;
+constexpr gpio_num_t LED_FEEDER_MOSI = 4_pin;
+constexpr gpio_num_t LED_FEEDER_SET = 16_pin;
+
+spi_device_handle_t init_line() {
+    spi_bus_config_t spi_bus = {
+        .mosi_io_num = LINE_MOSI,
+        .miso_io_num = LINE_MISO,
+        .sclk_io_num = LINE_SCK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 0,
+        .flags = 0
+    };
+    spi_bus_initialize( HSPI_HOST, &spi_bus, 0 ) || Die( "LINE: failed to initialize SPI bus" );
+
+    spi_device_handle_t dev;
+    spi_device_interface_config_t dev_cfg = {
+        .command_bits = 0,
+        .address_bits = 0,
+        .dummy_bits = 0,
+        .mode = 0,
+        .duty_cycle_pos = 0,
+        .cs_ena_pretrans = 0,
+        .cs_ena_posttrans = 0,
+        .clock_speed_hz = 1_MHz .value,
+//        .input_delay_ns = 0,
+        .spics_io_num = LINE_CS,
+        .flags = 0,
+        .queue_size = 2,
+        .pre_cb = 0,
+        .post_cb = 0
+    };
+    spi_bus_add_device( HSPI_HOST, &dev_cfg, &dev ) || Die( "LINE: failed to initialize SPI device" );
+    return dev;
+}
+
+std::array< bool, 8 > read_line( spi_device_handle_t dev )
+{
+    std::array< bool, 8 > values;
+    for ( int i = 0; i < 8; ++i ) {
+        spi_transaction_t trans{};
+        trans.tx_data[ 0 ] = 0b00000001;
+        trans.tx_data[ 1 ] = 0b10000000 | i << 4;
+        trans.length = 24;
+        trans.rxlength = 24;
+        trans.flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA;
+
+        spi_device_transmit( dev, &trans );
+
+        values[i] = ((trans.rx_data[1] & 0b11) << 8 | trans.rx_data[2]) > 512;
+    }
+    return values;
+}
+
+spi_device_handle_t init_led_feeder() {
+    spi_bus_config_t spi_bus = {
+        .mosi_io_num = LED_FEEDER_MOSI,
+        .miso_io_num = -1,
+        .sclk_io_num = LED_FEEDER_SCK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 0,
+        .flags = 0
+    };
+    spi_bus_initialize( VSPI_HOST, &spi_bus, 0 ) || Die( "LED FEEDER: failed to initialize SPI bus" );
+
+    spi_device_handle_t dev;
+    spi_device_interface_config_t dev_cfg = {
+        .command_bits = 0,
+        .address_bits = 0,
+        .dummy_bits = 0,
+        .mode = 0,
+        .duty_cycle_pos = 0,
+        .cs_ena_pretrans = 0,
+        .cs_ena_posttrans = 0,
+        .clock_speed_hz = 1_MHz .value,
+        .spics_io_num = -1,
+        .flags = 0,
+        .queue_size = 2,
+        .pre_cb = 0,
+        .post_cb = 0
+    };
+    spi_bus_add_device( VSPI_HOST, &dev_cfg, &dev ) || Die( "LED_FEEDER: failed to initialize SPI device" );
+    return dev;
+}
+
+void set_leds( spi_device_handle_t dev, uint8_t data ) {
+    spi_transaction_t trans{};
+    trans.length = 8;
+    trans.rxlength = 0;
+    trans.tx_data[ 0 ] = data;
+    trans.flags = SPI_TRANS_USE_TXDATA;
+    spi_device_transmit( dev, &trans );
+
+    static OutPin< LED_FEEDER_SET > set;
+    set.set( true );
+    set.set( false );
+}
+
+void set_leds( spi_device_handle_t dev, std::array< bool, 8 > data ) {
+    uint8_t val = 0;
+    int i = 0;
+    for ( auto v : data ) {
+        val |= v << i;
+        ++i;
+    }
+    set_leds( dev, val );
 }
 
 extern "C" void app_main()
@@ -131,10 +259,32 @@ extern "C" void app_main()
     ultrasonic_init_trig();
     UltrasonicDist dist;
     ultrasonic_measure_continuosly( dist );
-    std::cout << "before loop\n" << std::flush;
 
+    auto line_sensor = init_line();
+    auto led_feeder = init_led_feeder();
+
+    std::cout << "before loop\n" << std::flush;
     while ( true ) {
-        std::cout << "distance = " << dist.distance << " (" << dist.cnt << ')' << std::endl << std::flush;
-        std::this_thread::sleep_for( 1s );
+        if ( dist.distance < 100 && dist.distance > 0 )
+            break;
+        auto vals = read_line( line_sensor );
+        set_leds( led_feeder, vals );
+        std::copy( vals.begin(), vals.end(),
+                   std::experimental::make_ostream_joiner( std::cout, ", " ) );
+        std::cout << std::endl;
+
+        std::this_thread::sleep_for( 50ms );
+    }
+    std::cout << "start\n" << std::flush;
+    motorr_set( 40 );
+    motorl_set( 40 );
+    while ( true ) {
+        auto vals = read_line( line_sensor );
+        set_leds( led_feeder, vals );
+        std::copy( vals.begin(), vals.end(),
+                   std::experimental::make_ostream_joiner( std::cout, ", " ) );
+        std::cout << std::endl;
+
+        std::this_thread::sleep_for( 100ms );
     }
 }
